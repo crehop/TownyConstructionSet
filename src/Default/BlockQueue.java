66f59191397bbe749a28eeff4b73ed4ec7e023c3
chris <crehop2@gmail.com>
2014-09-02 10:43:18 -0700

    Util functions worked on

--- src/Default/BlockQueue.java
+++ src/Default/BlockQueue.java
@@ -1,5 +1,28 @@
 package Default;
 
-public class BlockQueue {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bukkit.Chunk;
+import org.bukkit.block.Block;
+
+public class BlockQueue {
+	Chunk newChunk;
+	Chunk copyChunk;
+	int x;
+	int y;
+	int z;
+	
+	public BlockQueue(Chunk copyChunk, Chunk newChunk){
+		this.copyChunk = copyChunk;
+		this.newChunk = newChunk;
+		x = this.copyChunk.getX();
+		z = this.copyChunk.getZ();
+		y = 0;
+	}
+	
+	public boolean builder(){
+		//cody to lay blocks and incriment x and y then z until full chunk built. returns true if built false if done
+		return false;
+	}
 }
---
+++ src/Default/TownyUtils.java
@@ -0,0 +1,53 @@
+package Default;
+
+import org.bukkit.entity.Player;
+
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.command.TownyAdminCommand;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.exceptions.TownyException;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownyObject;
+import com.palmergames.bukkit.towny.object.TownyUniverse;
+import com.palmergames.bukkit.towny.object.TownyWorld;
+
+public class TownyUtils {
+	public static Town getTownWhereStanding(Player player){
+		Town town = null;
+		try {
+			town = TownyUniverse.getTownBlock(player.getLocation()).getTown();
+		} catch (NotRegisteredException e) {
+		}
+		return town;
+	}
+	
+	public static boolean isTownOwner(Town town, Player player){
+		boolean canbuild = false;
+		boolean isResident = false;
+		Resident resident = null;
+		for(Resident res:town.getResidents()){
+			try {
+				if(TownyUniverse.getPlayer(res).getName().equals(player.getName())){
+					isResident = true;
+					resident = res;
+				}
+			} catch (TownyException e) {
+			}
+		}
+		if(isResident){
+			if(town.getMayor().getName().equals(resident.getName())){
+				canbuild = true;
+			}
+		}
+		return canbuild;
+	}
+	
+	public static boolean confirmKing(Player player){
+		 Resident resident = new Resident(player.getName());
+		 if(resident.isKing()){
+			 return true;
+		 }
+		 return false;
+	}
+}
